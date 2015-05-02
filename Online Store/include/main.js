7e3b03357bd2b165966fd59519dbc39b28cc487f
Teemo <Teemo@Teemo-PC>
2015-05-02 19:38:39 +0300

    commit

--- Online Store/include/main.js
+++ Online Store/include/main.js
@@ -1,14 +1,33 @@
 $(document).ready(function () {
+
     var products;
-    console.log("fuuuuuu");
     $.getJSON("json/products.json", function (data) {
-        console.log("ok");
         products = data;
-        console.log(products);
+        listProducts(products);
     }).fail(function (xhr, textStatus, errorThrown) {
         alert("error ajax" + xhr + " " + textStatus + " " + errorThrown);
     });
-    console.log("chrome sucks")
+
+
+    function listProducts(products) {
+        var box = "";
+        for (var i = 0; i < products.length; i++) {
+            var id = products[i]["id"];
+            var name = products[i]["name"];
+            var image = products[i]["colors"][0]["image"];
+            var price = products[i]["sizes"][0]["price"];
+            box = box + '<a href="product.html?id='+id+'"><div class="product-wrapper"><img src="' + image + '"><div class="title">' + name + '</div><div class="price">' + price + ' лв.</div></div></a>';
+        }
+        $("#container").html(box);
+
+        //hover product image
+        $(".product-wrapper").hover(function () {
+            $(this).find("img").stop().animate({opacity: 0.35}, 200);
+        }, function () {
+            $(this).find("img").stop().animate({opacity: 1}, 200);
+        });
+
+    }
 
 });
 
