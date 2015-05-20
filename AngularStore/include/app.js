d7f5a407ed8758152f8e6f3e11a1afab3b7bd9ed
Teemo <Teemo@Teemo-PC>
2015-05-20 23:33:52 +0300

    Angular Project

--- AngularStore/include/app.js
+++ AngularStore/include/app.js
@@ -14,12 +14,35 @@ app.config(['$routeProvider',
                 });
     }]);
 
-app.controller("productController", function ($scope, $routeParams) {
-    this.items = products;
-    $scope.product_id = $routeParams.product_id;
-    
-});
-
 app.controller("productsListController", function ($scope, $routeParams) {
     this.items = products;
+});
+
+app.controller("productController", function ($scope, $routeParams) {
+    $scope.product_id = $routeParams.product_id;
+    this.info;
+
+    //tyrsim produkta po ID
+    for (var i = 0; i < products.length; i++) {
+        if ($scope.product_id == products[i].id) {
+            this.info = products[i];
+            break;
+        }
+    }
+
+    //vzimame pyrvata defaultna snimka
+    this.currentImage = this.info.colors[0].image;
+    this.activeColor = this.info.colors[0].label;
+    
+    //funkciq za smqna na cvqt
+    this.changeColor = function (label) {
+        for(var i = 0; i < this.info.colors.length; i++){
+            if(label == this.info.colors[i].label){
+                this.currentImage = this.info.colors[i].image;
+                break;
+            }
+        }
+        this.activeColor = label;
+    }
+
 });
